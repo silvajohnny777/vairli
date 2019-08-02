0708632c68085382393ea7fe48ab9a0883477f52
silvajohnny777 <jonatha_silva2012@hotmail.com>
2019-08-02 20:24:50 -0300

    fodase

---
+++ build/precache-manifest.4148dfb65045f47093421c1d3d2e53d1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c6fcd65e920083bd26e9bfe6679e203",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20d105ccfd1d52b97466",
+    "url": "/static/css/main.68e610f7.chunk.css"
+  },
+  {
+    "revision": "2d82e251a5dc8ada9aaf",
+    "url": "/static/js/2.fe913967.chunk.js"
+  },
+  {
+    "revision": "20d105ccfd1d52b97466",
+    "url": "/static/js/main.ae51e0dd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
